ddc9ff32490f7b7dab8a3985af64f0e2b44717fb
sujal9594 <118432331+sujal9594@users.noreply.github.com>
2023-03-31 17:29:47 +0530

    Create Assignment_3_Loops.py

---
+++ Assignment_3_Loops.py
@@ -0,0 +1,6 @@
+start = int(input("Enter the starting number of the range: "))
+end = int(input("Enter the ending number of the range: "))
+
+for i in range(start, end+1):
+    if i > 0:
+        print(i)
